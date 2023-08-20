e74b6e635e74c555e164d8bbb3303bcc115da6bc
Rogennee98 <daniellevalenzuela41@gmail.com>
2023-08-19 23:38:17 -0800

    First commit - Commiting files

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#INstalling git link help https://stackoverflow.com/questions/63686488/error-git-is-not-recognized-as-the-name-of-a-cmdlet
+#Public Key error fix: https://stackoverflow.com/questions/2643502/git-how-to-solve-permission-denied-publickey-error-when-using-git
+#Public Key error in image from stack overflow:img.png
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi Daneille , {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
